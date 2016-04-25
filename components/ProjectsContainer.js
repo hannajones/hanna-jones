d1e54f2e523f7882d79ae977334735c16ceea2fe
hannajones <jones.hanna.m@gmail.com>
2016-04-25 17:48:26 -0400

    implemented basic store

--- components/ProjectsContainer.js
+++ components/ProjectsContainer.js
@@ -3,23 +3,11 @@ import Project from './Project';
 import Card from './Card';
 import { Link } from 'react-router';
 
-// projects state can probably live here instead
-
 class ProjectsContainer extends React.Component {
   constructor() {
     super()
     this.renderCards = this.renderCards.bind(this)
-    // this.loadSamples = this.loadSamples.bind(this)
   }
-  // componentWillMount() {
-  //   console.log('mounting')
-  //   this.loadSamples()
-  // }
-  // loadSamples() {
-  //   this.setState({
-  //     projects: require('../scripts/data.js')
-  //   })
-  // }
   renderCards(key) {
     return <Card key={key} index={key} project={this.props.projects[key]} />
   }
