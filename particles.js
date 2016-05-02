298909c107077ce52ffd2bace2261479cdfa2645
hannajones <jones.hanna.m@gmail.com>
2016-05-01 23:22:21 -0400

    particles.js working

--- particles.js
+++ particles.js
@@ -27,13 +27,13 @@ var pJS = function(tag_id, params){
         }
       },
       color: {
-        value: '#fff'
+        value: '#3c3c3c'
       },
       shape: {
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#3c3c3c'
         },
         polygon: {
           nb_sides: 5
@@ -67,7 +67,7 @@ var pJS = function(tag_id, params){
       line_linked: {
         enable: true,
         distance: 100,
-        color: '#fff',
+        color: '#3c3c3c',
         opacity: 1,
         width: 1
       },
