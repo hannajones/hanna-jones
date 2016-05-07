8cf81be690f0c590d8fc9269dfc34d5f3b2ef015
hannajones <jones.hanna.m@gmail.com>
2016-05-07 16:30:38 -0400

    projects displaying as cards

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,30 +1,6 @@
 import React from 'react';
 
 class Carousel extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      index: 0
-    }
-    this.setNextImage = this.setNextImage.bind(this)
-    this.setPreviousImage = this.setPreviousImage.bind(this)
-  }
-  setNextImage() {
-    if ((this.state.index + 1) < this.props.images.length) {
-      this.setState({index: this.state.index + 1})
-    }
-    else {
-      this.setState({index: 0})
-    }
-  }
-  setPreviousImage() {
-    if (this.state.index > 0) {
-      this.setState({index: this.state.index - 1})
-    }
-    else if (this.state.index === 0) {
-      this.setState({index: this.props.images.length - 1})
-    }
-  }
   render() {
     return (
       <div className="image-container center-align">
