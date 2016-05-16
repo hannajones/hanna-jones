9cbb16842deabebfbd1d182cc2c5f5a4414ced0e
hannajones <jones.hanna.m@gmail.com>
2016-05-16 16:12:46 -0400

    made some fixes to mobile display

--- components/Card.js
+++ components/Card.js
@@ -7,7 +7,7 @@ class Card extends React.Component {
   render() {
     return (
       <div className="card-image-holder">
-        <Link className="card-image-link" to={`/project/${this.props.project.id}`}>
+        <Link className="card-image-link" to={`/projects/${this.props.project.id}`}>
           <img className="card-border" src={this.props.project.images[0]} alt="project-image" />
           <span className="image-label"><mark>{this.props.project.title}</mark></span>
         </Link>
