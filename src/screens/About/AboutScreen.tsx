0d68f8df4066f841efb6e4d5e920f5b6ee6972e7
hannajones <jones.hanna.m@gmail.com>
2021-06-09 22:24:36 -0400

    cut down on the content

--- src/screens/About/AboutScreen.tsx
+++ src/screens/About/AboutScreen.tsx
@@ -26,13 +26,13 @@ const AboutScreen: React.FunctionComponent<AboutScreenProps> = () => (
       </div>
       <div className="about__description">
         <p className="about__description__paragraph">
-          My name is <span className="text--green">Hanna Jones</span>, and I'm a front-end web developer working primarily with React and TypeScript to build beautiful, unique websites and applications. I'm also an avid cyclist, techno enthusiast, and dark mode proponent.
+          My name is <span className="text--green">Hanna Jones</span>, and I'm a front-end web developer working primarily with React and TypeScript to build beautiful, unique websites and applications.
         </p>
         <p className="about__description__paragraph">
-          I was born & raised in <span className="text--blue">San Francisco</span>, but have called <span className="text--blue">Toronto</span> home for the past three years.
+          I was born & raised in <span className="text--blue">San Francisco</span>, but have called <span className="text--blue">"Toronto"</span> home for the past three years.
         </p>
         <p className="about__description__paragraph">
-          Explore this site to learn more about my <Link to={Routes.RESUME} className="text text--purple">experiences</Link> and <Link to={Routes.PROJECTS} className="text text--purple">projects</Link>, and feel free to <a className="text text--purple" href="mailto:hanna.m.jones@protonmail.com?Subject=Hello.">get in touch</a>.</p>
+          Explore this site to learn more about my <Link to={Routes.RESUME} className="text text--purple">experiences</Link> and feel free to <a className="text text--purple" href="mailto:hanna.m.jones@protonmail.com?Subject=Hello.">get in touch</a>.</p>
       </div>
     </div>
   </Card>
